6a75ee5e39383ac2503760c5ae26e9aad6312424
Anthony Rurup <tcrurup@gmail.com>
2019-09-07 01:33:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function scuberGreetingForFeet(feet){
   if (feet <= 400){
     return "This one is on me!";
-  } else if (feet < 2000){
+  } else if (feet <= 2000){
     return "I will gladly take your thirty bucks."
   } else {
     return "No can do."
